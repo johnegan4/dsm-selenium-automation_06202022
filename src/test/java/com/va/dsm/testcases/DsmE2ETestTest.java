d5dd7aebe0ef6c79d8a9dd0e43d861bf7dd7b378
johnegan4 <john.egan4@va.gov>
2022-11-14 16:29:33 -0600

    Initial commit

---
+++ src/test/java/com/va/dsm/testcases/DsmE2ETestTest.java
@@ -0,0 +1,11 @@
+package com.va.dsm.testcases;
+
+import org.testng.annotations.Test;
+
+public class DsmE2ETestTest {
+
+  @Test
+  public void Test001_DSM_Email_Compose_No_AttachmentTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
